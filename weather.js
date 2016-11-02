5c1462b3171385d4451070099fb4b3d64589d0b4
okputadora <okputadora@gmail.com>
2016-11-01 21:47:38 -0400

    Add weather API info

--- weather.js
+++ weather.js
@@ -3,20 +3,36 @@ $(document).ready(function(){
 var lat = 0;
 var lon = 0;
 var unitType = "imperial";
+var icon = "";
 console.log("Working...");
+
+var getWeather = function(data){
+  $("#location").text(data.name + ", " + data.sys.country);
+  $("#temp").text(Math.round(data.main.temp + " "));
+  icon = data.weather[0].icon;
+  $("#weather").text(data.weather[0].description);
+
+  if (unitType == "imperial"){
+    $("#units").text("  ℉");
+  }
+  else {$("#units").text(" ℃")};
+
+  $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + icon + ".png")
+};
   //get users location
   if (navigator.geolocation){
     console.log("in if statement");
     navigator.geolocation.getCurrentPosition(function(position){
       lat = position.coords.latitude;
       lon = position.coords.longitude;
-      console.log(lat);
-      console.log(lon);
       var apiKey = "ce505221f087812083715115cc1bfd0c";
-      var url = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +
+      var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +
                 "&units=" + unitType + "&appid=" + apiKey;
-      console.log(url);
+
+      //This won't work from a local file i.e. file:/// needs to be a live wp
+      $.getJSON(url, getWeather)
     });
   }
+
 
 });
